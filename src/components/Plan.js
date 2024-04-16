8c6054230ed6d47ad08d219639ccdbc2c359357c
karan-sohi <deepkaran2490@gmail.com>
2024-04-16 13:06:37 -0700

    Avoiding the submission of empty products and styled the plan page

--- src/components/Plan.js
+++ src/components/Plan.js
@@ -4,26 +4,55 @@ import PlanProduct from "./PlanProduct";
 import db from "../firebase";
 import { useState } from "react";
 import { useCollection } from "react-firebase-hooks/firestore";
-import { serverTimestamp } from 'firebase/firestore';
+import { serverTimestamp } from "firebase/firestore";
 import firebase from "firebase/compat/app";
-import SendIcon from '@mui/icons-material/Send';
+import SendIcon from "@mui/icons-material/Send";
 
 function Plan() {
   const [enterProduct, setEnterProduct] = useState("");
   const [products, setProducts] = useState([]);
-  const [productsSnapshot] = useCollection(db.collection("products").orderBy("timestamp", "asc"));
+  const [availableProducts, setAvailableProducts] = useState(false)
+  const [productsSnapshot] = useCollection(
+    db.collection("products").orderBy("timestamp", "asc")
+  );
 
-  const submitProductHandler = (event) => {
-    event.preventDefault();
-    db.collection("products").add({
-      name: enterProduct,
-      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
-      priority: 0,
-    });
-    setProducts([...products, { name: enterProduct }]);
-    setEnterProduct("");
+  // Delete unnecessary products from firestore database
+  // const [deleteSnapshot] = useCollection(
+  //   db.collection("products").where("priority", "==", 0)
+  // );
+
+  // deleteSnapshot?.docs.map((doc) => {
+  //   db.collection("products").doc(doc.id).delete();
+  // });
+
+  const checkProduct = productName => {
+    productsSnapshot.docs.map((doc) => {
+      if (doc.data().name == productName) {
+        setAvailableProducts(true);
+      }
+    })
+  }
+
+
+  const submitProductHandler = () => {
+
+    if (enterProduct.length <= 0 )  {
+      alert("Please enter product name");
+    } 
+    else if (checkProduct(enterProduct))  {
+      alert("Product already exists");
+    }
+    else {  
+      console.log("bad", enterProduct, "ab");
+      db.collection("products").add({
+        name: enterProduct,
+        timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+        priority: 0,
+      });
+      setProducts([...products, { name: enterProduct }]);
+      setEnterProduct("");
+    }
   };
-
 
   return (
     <div className="h-screen m-auto bg-slate-100">
@@ -33,24 +62,29 @@ function Plan() {
       <div className="h-3/5 pt-5 p-4 w-90 m-auto">
         <div className="grid grid-cols-3 gap-3">
           {productsSnapshot?.docs.map((product) => {
-            return <PlanProduct key={product.id} docId={product.id} product={product.data()}></PlanProduct>;
+            return (
+              <PlanProduct
+                key={product.id}
+                docId={product.id}
+                product={product.data()}
+              ></PlanProduct>
+            );
           })}
         </div>
       </div>
-      <div className="flex justify-center p-2 bg-white w-screen ">
+      <div className="flex justify-center bg-white w-screen ">
         <input
-          onKeyDown={(e) => {
-            if (e.key === "Enter") submitProductHandler(e);
-          }}
           onChange={(e) => setEnterProduct(e.target.value)}
           value={enterProduct}
           placeholder="Chicken"
-          className="p-3 m-2 w-4/5 border-2"
+          className="p-2 m-2 w-4/5 rounded-md border-2"
         ></input>
-        <div className="bg-red-300 m-2 justify-center">
-        <SendIcon ></SendIcon>
+        <div
+          className="bg-red-300 m-2 p-2 rounded-3xl"
+          onClick={submitProductHandler}
+        >
+          <SendIcon></SendIcon>
         </div>
-      
       </div>
     </div>
   );
